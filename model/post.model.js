6a4c27a2dc0203cb5ccefca197343dfb0ecc7abd
Gaspard Coulet <gaspard.coulet@mines-ales.org>
2021-09-24 11:56:37 +0200

    base classes

---
+++ model/post.model.js
@@ -0,0 +1,10 @@
+const https = require('https');
+
+let Post = class{
+    constructor(json){
+        this.content;
+        this.date;
+        this.titre;
+        this.nbLikes;
+    }
+}
---
+++ model/user.model.js
@@ -0,0 +1,19 @@
+const https = require('https');
+
+let User = class{
+    constructor(){
+
+        this.id;
+        this.name;
+        this.surname;
+        this.birthdate;
+        this.postcode;
+        this.photo;
+        this.Posts;
+        this.email;
+        this.username;
+        this.accountCreation;
+        this.phoneNumber;
+        this.gender;
+    }
+}
