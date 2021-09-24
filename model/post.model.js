8a6c397e527c24e7d62fee1349b47cac81f671d5
senetal <alex.senet38@gmail.com>
2021-09-24 15:36:32 +0200

    Tested post generation and removed dead code.

--- model/post.model.js
+++ model/post.model.js
@@ -9,36 +9,9 @@ let Post = class{
         this.content = content;
         this.date = date;
         this.nbLikes = nbLikes;
-        this.userid = userId;
+        this.userId = userId;
     }
 
-    getData() {
-        const req = https.request('https://randomuser.me/api', res => {
-            let data = '';
-            res.on('data', (chunk) => {
-                data = data + chunk.toString();
-            });
-            res.on('end', () => {
-                const body = JSON.parse(data).results[0];
-                this.id = body.login.uuid;
-                this.name = body.name.first;
-                this.surname = body.name.last;
-                this.postcode = body.location.postcode;
-                this.accountCreation = body.registered.date;
-                this.birthdate = body.dob.date;
-                this.photo = body.picture.medium;
-                this.username = body.login.username;
-                this.email = body.email;
-                this.phoneNumber = body.cell;
-                this.gender = body.gender;
-                this.Posts = [];
-            });
-        });
-        req.on('error', (error) => {
-            console.log('An error', error);
-        });
-        req.end();
-    }
 }
 
 module.exports=Post
