45e9d53a3cfed37c0b1f20ec52f83cb9314024a2
Florian <florian.girardin42@gmail.com>
2021-09-24 15:01:40 +0200

    recuperation des posts depuis un fichier json

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var controller = require('../controller.js');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var listPosts = controller.getAllPost();
+  res.render('index', { title: 'Express', listPosts: listPosts });
 });
 
 module.exports = router;
