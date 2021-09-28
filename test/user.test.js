58bcb15e87a0e726f4fb69f9a9b69fde24f54482
BastLast <bastien.last87@gmail.com>
2021-09-28 23:58:51 +0200

    [not functionnal] tentatives de tests supplémentaires

--- test/user.test.js
+++ test/user.test.js
@@ -17,4 +17,27 @@ chai.should();describe("user test", () => {
         });
     });
 
+    describe("GET a new user", () => {
+
+        // Test
+        it("should be able to generate a new user", (done) => {
+            chai.request(app)
+                .get('/users/new')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.text.should.be.a('string');
+                    done();
+                });
+        });
+
+        it("should be a different user each time", (done) => {
+            chai.request(app)
+                .get('/users/id?userId=b0bc44d1-85e9-423e-ac20-ec33556efbcc')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.text.should.be.a('string');
+                    done();
+                });
+        });
+    });
 });
