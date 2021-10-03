bf246943d9651bbf34b45cefdd0e5d4c9b4b44b0
BastLast <bastien.last87@gmail.com>
2021-10-03 20:37:47 +0200

    ajout de test & augmentation du timeout pour les tests

--- test/user.test.js
+++ test/user.test.js
@@ -2,7 +2,8 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const app = require("../app")
 var controller = require('../controller.js');
-
+const assertArrays = require('chai-arrays');
+chai.use(assertArrays);
 chai.use(chaiHttp);
 chai.should();
 describe("user test", () => {
@@ -58,4 +59,23 @@ describe("user test", () => {
             chai.expect(nbUserafter).to.equal(nbUserbefore + 1);
         });
     });
+
+    describe("create the database", () => {
+        it("should add a few users in the database", async () => {
+            await controller.createBaseUser();
+            userList = require('../userList.json');
+            let nbUserafter = userList.users.length;
+            chai.expect(nbUserafter).to.be.above(0);
+        });
+    });
+
+    describe("get all existing posts", () => {
+        it("should return all existing posts in the database", (done) => {
+            let posts = controller.getAllPost();
+            chai.expect(posts[0]).to.not.be.undefined;
+            chai.expect(posts).to.be.array();
+            done();
+        });
+    });
+
 });
