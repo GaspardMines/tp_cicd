69f0d3b3fb24d0d5eb9e03d26a8a22ccf1365ce4
BastLast <bastien.last87@gmail.com>
2021-10-03 20:20:32 +0200

    ajout de tests

--- test/user.test.js
+++ test/user.test.js
@@ -12,7 +12,7 @@ describe("user test", () => {
             let userList = require('../userList.json');
             let userId = userList.users[0].id;
             chai.request(app)
-                .get('/users/id?userId='+userId)
+                .get('/users/id?userId=' + userId)
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.text.should.be.a('string');
@@ -30,8 +30,32 @@ describe("user test", () => {
             controller.updateLike(userId, id);
             userList = require('../userList.json');
             let likeafter = userList.users[0].Posts[0].nbLikes;
-            chai.expect(likeafter).to.equal(likebefore+1);
+            chai.expect(likeafter).to.equal(likebefore + 1);
             done();
+        });
+    });
+
+
+    describe("add post", () => {
+        it("should add a post to a specific user", async () => {
+            let userList = require('../userList.json');
+            let userId = userList.users[0].id;
+            let nbPostbefore = userList.users[0].Posts.length;
+            await controller.addPostByUserId(userId);
+            userList = require('../userList.json');
+            let nbPostafter = userList.users[0].Posts.length;
+            chai.expect(nbPostafter).to.equal(nbPostbefore + 1);
+        });
+    });
+
+    describe("add user", () => {
+        it("should add a user in the database", async () => {
+            let userList = require('../userList.json');
+            let nbUserbefore = userList.users.length;
+            await controller.addUser();
+            userList = require('../userList.json');
+            let nbUserafter = userList.users.length;
+            chai.expect(nbUserafter).to.equal(nbUserbefore + 1);
         });
     });
 });
